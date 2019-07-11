9b369328a15fb27e16c1d3d72af1b10fd23bfb91
zvvysotskaya <zhannavyvy@gmail.com>
2019-07-11 14:36:20 -0400

    added bootstrap and js files

---
+++ js/contentPages/education.js
@@ -0,0 +1,20 @@
+$(function(){
+    $('#b_education').click(function(){
+        $('#page_content').html(
+            '<div class="container-fluid">'+
+            '<h1 class="text-center text-info font-weight-bold mt-md-5 mt-3">'+
+
+            'Education, Certification, and Personal Development'+
+
+            '<h4 class="text-center ">Sorry, the site undergos changes</h4>'+//need to be removed!!!!
+
+            '<h1>'+
+            '</div>'+
+            '<article>'+
+            '<h4 class="font-weight-bold mt-3 mt-md-5 p-3">EDUCATION</h4>'+
+            '</article>'
+
+
+    );//the enf of HTML method
+});//the end of the click event
+});// the end of document.ready event handler
---
+++ js/contentPages/skills.js
@@ -0,0 +1,5 @@
+$(function(){
+    $('#b_skills').click(function(){
+        $('#page_content').html('<h3>Sorry, my site undergoes changes. The <em>Skills</em> page is coming soon</h3>');
+    });
+});

d8ef4350d498a81073541f054b1ec8508b63f9db
zvvysotskaya <zhannavyvy@gmail.com>
2019-07-11 20:25:06 -0400

    the education page added

--- js/contentPages/education.js
+++ js/contentPages/education.js
@@ -2,16 +2,44 @@ $(function(){
     $('#b_education').click(function(){
         $('#page_content').html(
             '<div class="container-fluid">'+
-            '<h1 class="text-center text-info font-weight-bold mt-md-5 mt-3">'+
-
-            'Education, Certification, and Personal Development'+
-
-            '<h4 class="text-center ">Sorry, the site undergos changes</h4>'+//need to be removed!!!!
-
-            '<h1>'+
-            '</div>'+
+                '<h1 class="text-center text-info font-weight-bold pt-md-5 mt-md-5 mt-3">'+
+                    'Education, Certification, and Professional Development'+
+                '</h1>'+
+            '</div><!--.container-fluid-->'+
             '<article>'+
-            '<h4 class="font-weight-bold mt-3 mt-md-5 p-3">EDUCATION</h4>'+
+            '<div class="container">'+
+                '<div class="row">'+
+                    '<div class="col-md-2"></div>'+
+                    '<div class="col-md-8">'+
+                        '<h4 class="font-weight-bold mt-3 mt-md-5 p-3">EDUCATION</h4>'+
+                        '<p class="font-weight-bold ">2016 - 2018</p>'+
+                        '<p class="font-weight-bold">Associate in Science in Computer/Web Programming and Analysis</p>'+
+                        '<p>St. Petersburg College, St. Petersburg, FL, USA. Completed 63 credits, GPA 3.81</p>'+
+                        '<h4 class="font-weight-bold mt-3 mt-md-5 p-3">CERTIFICATION AND PROFESSIONAL DEVELOPMENT</h4>'+
+                        '<p class="font-weight-bold ">2018<br>Certificate</p>'+
+                        '<p class="font-weight-bold ">Computer Programmer</p>'+
+                        '<p class="pb-4">St. Petersburg College, St. Petersburg, FL, USA.</p>'+
+                        '<p class="font-weight-bold ">2018<br>Certificate</p>'+
+                        '<p class="font-weight-bold ">Computer Programming Specialist</p>'+
+                        '<p class="pb-4">St. Petersburg College, St. Petersburg, FL, USA.</p>'+
+                        '<p class="font-weight-bold ">2019<br>Certificate</p>'+
+                        '<p>Microsoft - DEV208x Introduction to jQuery, edX, Inc</p>'+
+                        '<p class="pb-4"><a href="https://courses.edx.org/certificates/fa562e14408146e2b262eab50d850e4c" class="text-info">See credential</a></p>'+
+                        '<p class="font-weight-bold ">2019<br>Certificate</p>'+
+                        '<p>Microsoft - DEV280x Building Functional Prototypes using Node.js. edX, Inc.</p>'+
+                        '<p class="pb-4"><a href="https://courses.edx.org/certificates/47ee33b3e655402d93358c14c3db8703" class="text-info">See credential</a></p>'+
+                        '<p class="font-weight-bold ">2019<br>Certificate</p>'+
+                        '<p>Microsoft - DEV279 Building Interactive Prototypes Using JavaScript. edX, Inc.</p>'+
+                        '<p class="pb-4"><a href="https://courses.edx.org/certificates/967a7a025ca74155ba64e6383e9542f5" class="text-info">See credential</a></p>'+
+                        '<p class="font-weight-bold">2019 <br>Certificate</p>'+
+                        '<p>Microsoft - DEV284x Designing Technical Solution, edX Inc.</p>'+
+                        '<p class="pb-4"><a href="https://courses.edx.org/certificates/3b5d63d596774231a1c68283575bc7d1" class="text-info">See credential</a></p>'+
+                        '<p class="font-weight-bold">2019 <br>Self-taught programming languages:</p>'+
+                        '<p class="pb-4">PHP, Vue, Bootstrap, Laravel Framework, LAMP</p>'+
+                    '</div><!--.col-md-8-->'+
+                    '<div class="col-md-2"></div>'+
+                '</div><!--.row-->'+
+            '</div><!--.container-->'+
             '</article>'
 
 
--- js/contentPages/home.js
+++ js/contentPages/home.js
@@ -3,25 +3,25 @@ $(function(){
     $('#b_home').click(function(){
     $('#page_content').html(
 
-        '<div class="container mb-5 mt-5">'+
-            '<div class="row">'+
+        '<div class="container">'+
+            '<div class="row p-3 mb-3 mt-5">'+
                 '<div class="col">'+
                     '<h1 class="text-center text-info font-weight-bold">Hello!</h1>'+
                     '<h2 class="text-center">I am Zhanna</h2>'+
-                    '<h5 class="text-center text-dark mb-3">Software / Web developer</h5>'+
+                    '<h5 class="text-center text-dark mb-md-3">Software / Web developer</h5>'+
                 '</div>'+
             '</div><!-- .row -->'+
         '</div><!-- .container -->'+
         '<div class="container">'+
-            '<div class="row">'+
-                '<div class="col-md-6 p-5">'+
+            '<div class="row p-3">'+
+                '<div class="col-md-6">'+
                     '<h2 class="text-center text-info mb-4 font-weight-bold ">Introduction</h2>'+
                     '<p>'+
                         'My passion is coding. I love challenge and constantly looking for how to develop '+
                         ' a high quality code that is thoroughly tested, easy to read, and simpler to maintain.'+
                     '</p>'+
                 '</div><!-- .col-md-6 -->'+
-                '<div class="col-md-6 p-5">'+
+                '<div class="col-md-6">'+
                     '<h2 class="text-center text-info mb-4 font-weight-bold">Programming Interest</h2>'+
                     '<ul class="">'+
                         '<li>'+

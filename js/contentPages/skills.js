471d0f892c63dd5f529ef4116e6d499652c2f9b3
zvvysotskaya <zhannavyvy@gmail.com>
2019-07-14 00:44:25 -0400

    the skills page is changed

--- js/contentPages/skills.js
+++ js/contentPages/skills.js
@@ -9,24 +9,26 @@ $(function(){
             '<article>'+
             '<div class="container">'+
 
-
             '<p class="text-center font-weight-bold">Programming Languages</p>'+
             '<p class="text-center mb-5">PHP, Bootstrap, HTML5, CSS3, jQuery, JavaScript, SQL, C#, Java, and Python</p>'+
             '<p class="text-center font-weight-bold">DBMS</p>'+
             '<div class="row">'+
+
             '<div class="col-md-6 mb-5">'+
             '<p>I am using phpMyAdmin software tool to run mySql over the web.</p>'+
-            '<img src="images/mysql.jpg" class="img-fluid">'+
-
+            '<img src="images/mysql.jpg" class="d-block img-fluid">'+
             '</div><!--.col-6-->'+
 
-            '<div class="col-md-6">'+
-            '<p>In order to run SQLite database I am using command line.</p>'+
-            '<img src="images/sqlite.jpg" class="img-fluid">'+
+            '<div class="col-md-6 text-center">'+
+            '<p>To run the SQLite shell, I am using the command prompt.</p>'+
+            '<img src="images/sqlite.jpg" class="d-block img-fluid">'+
             '</div><!--.col-6-->'+
-
             '</div><!--.row-->'+
             '</div><!--.container-->'+
+
+            '<div class="container">'+
+            '<p class="text-center font-weight-bold">IDE</p>'+
+            '</div>'+
             '</article>'
         );
     });

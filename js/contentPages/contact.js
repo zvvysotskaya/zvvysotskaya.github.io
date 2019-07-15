618155f16a88be13b4ace122eeb8157900399e64
zvvysotskaya <zhannavyvy@gmail.com>
2019-07-15 01:21:09 -0400

    adding contact page

---
+++ js/contentPages/contact.js
@@ -0,0 +1,45 @@
+$(function(){
+    $('#b_contact').click(function(){
+        $('#page_content').html(
+            '<div class="container-fluid">'+
+                '<h1 class="text-center text-info font-weight-bold pt-md-3 mt-md-5 mt-3 mb-2 mb-md-5">'+
+                    'Contact'+
+                '</h1>'+
+            '</div><!--.container-fluid-->'+
+            '<article>'+
+            '<div class="container">'+
+            '<div class="row">'+
+            '<div class="col-12 col-md-4 col-lg-4 mb-4">'+
+                '<p class="text-center font-weight-bold">Location:</p>'+
+                '<p class="text-center">Tampa Bay Area, Florida, USA</p>'+
+            '</div>'+
+            '<div class="col-12 col-md-4 col-lg-4 mb-4">'+
+                '<p class="text-center font-weight-bold">Email:</p>'+
+                '<p class="text-center"><a href="mailto:zhannavyvy@gmail.com">zhannavyvy@gmail.com</a></p>'+
+            '</div>'+
+            '<div class="col-12 col-md-4 col-lg-4 mb-4">'+
+                '<p class="text-center font-weight-bold">Social Media:</p>'+
+
+                '<p class="text-center">'+
+                    '<a href="https://www.linkedin.com/in/zhanna-vysotskaya-47810315b/" class="linkedin mr-0 mr-md-2 mt-md-3 ">'+
+                                '<i class="fa fa-linkedin fa-2x"></i>'+
+                    '</a>'+
+
+
+                    '<a href="https://github.com/zvvysotskaya" class="github ml-0 ml-md-2  mt-md-3">'+
+                                '<i class="fa fa-github fa-2x"></i>'+
+                            '</a>'+
+                            '</p>'+
+
+
+            '</div>'+
+            '</div><!--.row-->'+
+            '</div><!--.container-->'+
+
+
+
+            '</article>'
+        );
+    });
+
+});
--- js/contentPages/skills.js
+++ js/contentPages/skills.js
@@ -2,7 +2,7 @@ $(function(){
     $('#b_skills').click(function(){
         $('#page_content').html(
             '<div class="container-fluid">'+
-                '<h1 class="text-center text-info font-weight-bold pt-md-3 mt-md-5 mt-3 mb-5">'+
+                '<h1 class="text-center text-info font-weight-bold pt-md-3 mt-md-5 mt-3 mb-2 mb-md-5">'+
                     'Technical Skills'+
                 '</h1>'+
             '</div><!--.container-fluid-->'+
@@ -10,9 +10,9 @@ $(function(){
             '<div class="container">'+
 
             '<p class="text-center font-weight-bold">Programming Languages</p>'+
-            '<p class="text-center mb-5">PHP, Bootstrap, HTML5, CSS3, jQuery, JavaScript, SQL, C#, Java, and Python</p>'+
+            '<p class="text-center mb-md-5 mb-1">PHP, Bootstrap, HTML5, CSS3, jQuery, JavaScript, SQL, C#, Java, and Python</p>'+
             '<p class="text-center font-weight-bold">DBMS</p>'+
-            '<div class="dbs d-flex justify-content-center mb-5">'+
+            '<div class="dbs d-flex justify-content-center mb-md-5 mb-1">'+
             '<ul class="list-inline">'+
             '<li class="text-info">mySQL</li>'+
             '<li class=" text-info"> SQLite</li>'+
@@ -20,14 +20,14 @@ $(function(){
             '</div>'+
             '<div class="row">'+
 
-            '<div class="col-md-6 mb-5">'+
+            '<div class="col-md-6 mb-md-5 mb-2">'+
             '<p>I am using phpMyAdmin software tool to run mySql over the web.</p>'+
-            '<img src="images/mysql.jpg" class="d-block img-fluid img-thumbnail d-block mx-auto">'+
+            '<img src="images/mysql.jpg" alt="phpmyadmin" class="d-block img-fluid img-thumbnail d-block mx-auto">'+
             '</div><!--.col-md-6-->'+
 
-            '<div class="col-md-6">'+
+            '<div class="col-md-6 mb-2">'+
             '<p>To run the SQLite shell, I am using the command prompt.</p>'+
-            '<img src="images/sqlite.jpg" class="d-block img-fluid img-thumbnail d-block mx-auto">'+
+            '<img src="images/sqlite.jpg" alt="the command prompt" class="d-block img-fluid img-thumbnail d-block mx-auto">'+
             '</div><!--.col-md-6-->'+
             '</div><!--.row-->'+
             '</div><!--.container-->'+
@@ -36,25 +36,36 @@ $(function(){
             '<p class="text-center font-weight-bold">IDE</p>'+
             '<div class="ide d-flex justify-content-center">'+
             '<ul class="list-inline">'+
-            '<li class="text-info mb-5">Atom</li>'+
+            '<li class="text-info mb-md-5 mb-1">Atom</li>'+
             '<li class=" text-info">Notepad++</li>'+
             '<li class=" text-info">Visual Studio 16.1.6</li>'+
             '</ul>'+
             '</div><!--.ide-->'+
-            '<div class="row">'+
+            '<div class="row mb-md-5">'+
 
             '<div class="col-12 col-md-4 mb-2">'+
-            '<img src="images/atom.jpg" class="img-fluid img-thumbnail d-block mx-auto">'+
+            '<img src="images/atom.jpg" alt="atom" class="img-fluid img-thumbnail d-block mx-auto">'+
             '</div>'+
 
             '<div class="col-12 col-md-4 mb-2">'+
-            '<img src="images/notepad.jpg" class="img-fluid img-thumbnail d-block mx-auto">'+
+            '<img src="images/notepad.jpg" alt="notepad" class="img-fluid img-thumbnail d-block mx-auto">'+
             '</div>'+
             '<div class="col-12 col-md-4 mb-2">'+
-            '<img src="images/visualStudio.jpg" class="img-fluid img-thumbnail d-block mx-auto">'+
+            '<img src="images/visualStudio.jpg" alt="visual studio" class="img-fluid img-thumbnail d-block mx-auto">'+
             '</div>'+
 
             '</div><!--.row-->'+
+            '</div><!--.container-->'+
+
+            '<div class="container">'+
+            '<p class="text-center font-weight-bold">App Development Tools</p>'+
+            '<div class="ide d-flex justify-content-center">'+
+            '<ul class="list-inline">'+
+            '<li class="text-info mb-md-5 mb-1">XAMPP</li>'+
+            '<li class=" text-info">Laravel Valet</li>'+
+            '<li class=" text-info">Node.js</li>'+
+            '<li class=" text-info">React</li>'+
+            '</ul>'+
             '</div><!--.container-->'+
             '</article>'
         );

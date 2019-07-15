9f04745b7ae17dae37999e776219da3454d28a3b
zvvysotskaya <zhannavyvy@gmail.com>
2019-07-15 14:52:16 -0400

    adding js file

---
+++ js/contentPages/navHighlight.js
@@ -0,0 +1,39 @@
+$(function(){
+
+        $('#b_skills').click(function(){
+            $('#b_skills').css({'background-color':'#004d4d','color':'#ffffff','border-radius':'0'}).removeClass('bg-info');
+            $('#b_home').addClass('bg-info');
+            $('#b_education').addClass('bg-info');
+            $('#b_projects').addClass('bg-info');
+            $('#b_contact').addClass('bg-info');
+        });
+        $('#b_home').click(function(){
+            $('#b_home').css({'background-color':'#004d4d', 'color':'#ffffff','border-radius':'0'}).removeClass('bg-info');
+            $('#b_skills').addClass('bg-info');
+            $('#b_education').addClass('bg-info');
+            $('#b_projects').addClass('bg-info');
+            $('#b_contact').addClass('bg-info');
+        });
+        $('#b_education').click(function(){
+            $('#b_education').css({'background-color':'#004d4d', 'color':'#ffffff', 'border-radius':'0'}).removeClass('bg-info');
+            $('#b_home').addClass('bg-info');
+            $('#b_skills').addClass('bg-info');
+            $('#b_projects').addClass('bg-info');
+            $('#b_contact').addClass('bg-info');
+        });
+        $('#b_projects').click(function(){
+            $('#b_projects').css({'background-color':'#004d4d', 'color':'#ffffff', 'border-radius':'0'}).removeClass('bg-info');
+            $('#b_home').addClass('bg-info');
+            $('#b_skills').addClass('bg-info');
+            $('#b_education').addClass('bg-info');
+            $('#b_contact').addClass('bg-info');
+        });
+
+        $('#b_contact').click(function(){
+            $('#b_contact').css({'background-color':'#004d4d', 'color':'#ffffff', 'border-radius':'0'}).removeClass('bg-info');
+            $('#b_home').addClass('bg-info');
+            $('#b_skills').addClass('bg-info');
+            $('#b_education').addClass('bg-info');
+            $('#b_projects').addClass('bg-info');
+        });
+        });
